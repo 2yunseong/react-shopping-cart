235272e0d5176cab9b3480500313e897c8998865
2yunseong <dbsdltjd3701@naver.com>
2023-05-22 16:47:02 +0900

    배포 환경에서 msw 사용을 위해 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { worker } from './mocks/browser';
 import Router from './Router';
 
-if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/browser');
-  worker.start();
-}
+const main = async () => {
+  if (process.env.NODE_ENV === 'development') {
+    worker.start();
+    return;
+  }
+
+  if (window.location.pathname === '/react-shopping-cart') {
+    window.location.pathname = '/react-shopping-cart/';
+    return;
+  }
+
+  await worker.start({
+    serviceWorker: {
+      url: '/react-shopping-cart/mockServiceWorker.js',
+    },
+  });
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,3 +30,5 @@ root.render(
     <Router />
   </React.StrictMode>
 );
+
+main();
